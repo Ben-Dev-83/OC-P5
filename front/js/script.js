7ff9d5eaa1b5febc10b7871a5eafb3f222731aee
Benjamin Bonvicini <benjamin.bonvicini83@gmail.com>
2022-09-17 09:15:10 +0200

    apparition des canapés via l'api

---
+++ front/js/script.js
@@ -0,0 +1,40 @@
+const callApi = async function () {
+
+  fetch("http://localhost:3000/api/products")
+  .then(response => response.json())
+  .then(data => {
+    
+    for(items of data) {
+      
+      
+      const itemContainer = document.querySelector('#items');
+      const a = document.createElement("a");
+      a.href = `./product.html?id=${items._id}`
+      itemContainer.appendChild(a);
+      
+      const article = document.createElement("article");
+      a.appendChild(article);
+      
+      const img = document.createElement("img");
+	    img.src = items.imageUrl;
+	    img.alt = items.altTxt;
+	    article.appendChild(img);
+      
+      const title = document.createElement("h3");
+      title.classList.add('productName')
+	    title.innerText = items.name;
+	    article.appendChild(title);
+      
+      const p = document.createElement('p')
+      p.classList.add('description')
+      p.innerText = items.description;
+      article.appendChild(p);
+    }
+    
+  })
+}
+callApi()
+  
+
+
+
