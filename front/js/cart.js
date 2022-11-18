9770f6cfa2c82c226aede9adfe78f4e41cd2be0c
Benjamin Bonvicini <benjamin.bonvicini83@gmail.com>
2022-11-18 16:48:18 +0100

    suppression items, formulaire regex, envoi formulaire

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,18 +1,28 @@
 let getItem = JSON.parse(localStorage.getItem('items'));
-let numberInit= 0
-let initPrice =0
-
+getItem.sort((a, b) => {
+    if (a.id < b.id) {
+      return -1;
+    }
+    if (a.id > b.id) {
+      return 1;
+    }
+    return 0
+})
+let numberInit = 0
+let initPrice = 0
 for(let item of getItem) {
+
     fetch(`http://localhost:3000/api/products/${item.id}`)
     .then(response => response.json())
     .then(data => {
+
     let section = document.getElementById("cart__items")
     let article = document.createElement("article")
 
     article.classList.add("cart__item")
     section.appendChild(article)
-    article.dataId =item.id
-    article.dataColor =item.color
+    article.dataId = item.id
+    article.dataColor = item.color
 
     let divImg = document.createElement("div")
     divImg.classList.add("cart__item__img")
@@ -31,31 +41,31 @@ for(let item of getItem) {
     contentDescription.classList.add("cart__item__content__description")
     divContent.appendChild(contentDescription)
 
-    let itemTitle =document.createElement("h2")
-    itemTitle.innerText = data.name
-    contentDescription.appendChild(itemTitle)
+    let itemTitle =document.createElement("h2");
+    itemTitle.innerText = data.name;
+    contentDescription.appendChild(itemTitle);
 
-    let color =document.createElement("p")
-    color.innerText = item.color
-    contentDescription.appendChild(color)
+    let color =document.createElement("p");
+    color.innerText = item.color;
+    contentDescription.appendChild(color);
 
-    let quantity =document.createElement("p")
-    quantity.innerText = `${data.price} €`
-    contentDescription.appendChild(quantity)
+    let quantity =document.createElement("p");
+    quantity.innerText = `${data.price} €`;
+    contentDescription.appendChild(quantity);
 
-    let Contentsettings = document.createElement("div")
-    Contentsettings.classList.add("cart__item__content__settings")
-    divContent.appendChild(Contentsettings)
+    let Contentsettings = document.createElement("div");
+    Contentsettings.classList.add("cart__item__content__settings");
+    divContent.appendChild(Contentsettings);
     
-    let Contentquantity =document.createElement("p")
-    Contentquantity.classList.add("cart__item__content__settings__quantity")
-    quantity.innerText = `Qté: ${item.quantity}`
-    Contentsettings.appendChild(Contentquantity)
+    let Contentquantity =document.createElement("p");
+    Contentquantity.classList.add("cart__item__content__settings__quantity");
+    quantity.innerText = `Qté: ${item.quantity}`;
+    Contentsettings.appendChild(Contentquantity);
 
-    let price =document.createElement("p")
-    Contentquantity.classList.add("cart__item__content__settings__quantity")
-    quantity.innerText = `${data.price} €`
-    Contentquantity.append(price)
+    let price =document.createElement("p");
+    Contentquantity.classList.add("cart__item__content__settings__quantity");
+    quantity.innerText = `${data.price} €`;
+    Contentquantity.append(price);
 
     let addQuantity =document.createElement("input");
     addQuantity.name = "itemQuantity";
@@ -67,7 +77,7 @@ for(let item of getItem) {
     Contentsettings.appendChild(addQuantity);
     
     let totalQty = document.getElementById("totalQuantity");
-    const total = getItem.map(item =>item.quantity).reduce((pre, curr) => pre +curr, 0);
+    const total = getItem.map(item => item.quantity).reduce((pre, curr) => pre +curr, 0);
     totalQty.innerHTML = total;
 
     let totalPrice = document.getElementById("totalPrice");
@@ -98,14 +108,121 @@ for(let item of getItem) {
     let deleteItem = document.createElement("p")
     deleteItem.classList.add("deleteItem")
     deleteItem.innerText = "Supprimer"
+    let dataId = article.dataset.id = item.id
+    let dataColor = article.dataset.color = item.color
     divDelete.appendChild(deleteItem)
-    deleteItem.addEventListener("click", (e) =>{
+    deleteItem.addEventListener("click", () => {
         deleteItem.closest(".cart__item")
-        console.log(localStorage.removeItem("items[0]"))
         section.removeChild(article)
-        localStorage.removeItem()
+        let itemsFilter = getItem.filter(cart => cart.id !== dataId || cart.color !== dataColor)
+        let newItem = itemsFilter
+        localStorage.setItem('items', JSON.stringify(newItem));
+        item.price = data.price
+        item.subTotal = item.price
+        let totalItemPrice = itemsFilter.map(item => item.subTotal).reduce((pre, curr) => pre + curr, 0)
+        let totalQuantity = itemsFilter.map(item => item.quantity).reduce((pre, curr) => pre + curr, 0)
+        totalPrice.innerHTML = totalItemPrice 
+        totalQty.innerHTML = totalQuantity;
+        location.reload();
     })
-    
-    
+
+    let firstName = document.getElementById('firstName')
+    let firstNameErrorMsg = document.getElementById('firstNameErrorMsg')
+    firstName.addEventListener('blur', (e) => {
+        let text = e.target.value
+        let regex = new RegExp(/^[a-zA-Zéêëèîïâäçù ,'-]{3,20}$/)
+        if(!regex.test(text)) {
+            firstNameErrorMsg.innerHTML = "Veuillez saisir votre prénom"
+        }
+        else {
+            firstNameErrorMsg.innerHTML = ""
+        }
+    })
+
+    let lastName = document.getElementById('lastName')
+    let lastNameErrorMsg = document.getElementById('lastNameErrorMsg')
+    lastName.addEventListener('blur', (e) => {
+        let text = e.target.value
+        let regex = new RegExp(/^[a-zA-Zéêëèîïâäçù ,'-]{3,20}$/)
+        if(!regex.test(text)) {
+            lastNameErrorMsg.innerHTML = "Veuillez saisir votre nom"
+        }
+        else {
+            lastNameErrorMsg.innerHTML = ""
+        }
+    })
+
+    let address = document.getElementById('address')
+    let addressErrorMsg = document.getElementById('addressErrorMsg')
+    address.addEventListener('blur', (e) => {
+        let text = e.target.value
+        let regex = new RegExp(/^[0-9]{1,3}[a-zA-Zéêëèîïâäçù ,'-]{3,30}$/)
+        if(!regex.test(text)) {
+            addressErrorMsg.innerHTML = "Veuillez saisir votre adresse"
+        }
+        else {
+            addressErrorMsg.innerHTML = ""
+        }
+    })
+
+    let city = document.getElementById('city')
+    let cityErrorMsg = document.getElementById('cityErrorMsg')
+    city.addEventListener('blur', (e) => {
+        let text = e.target.value
+        let regex = new RegExp(/^[a-zA-Zéêëèîïâäçù ,'-]{3,20}$/)
+        if(!regex.test(text)) {
+            cityErrorMsg.innerHTML = "Veuillez saisir votre ville"
+        }
+        else {
+            cityErrorMsg.innerHTML = ""
+        }
+    })
+
+    let email = document.getElementById('email')
+    let emailErrorMsg = document.getElementById('emailErrorMsg')
+    email.addEventListener('blur', (e) => {
+        let text = e.target.value
+        let regex = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+        console.log(regex.test(text))
+        if(!regex.test(text)) {
+            emailErrorMsg.innerHTML = "Email invalide"
+        }
+        else {
+            emailErrorMsg.innerHTML = ""
+        }
+    })
+    let order = document.getElementById('order')
+    order.addEventListener('click', (e) => {
+        e.preventDefault(e)
+        let products = getItem.map(product => product.id)
+        let contact = {
+            firstName: firstName.value,
+            lastName: lastName.value,
+            address: address.value,
+            city: city.value,
+            email: email.value
+        }
+        
+        fetch("http://localhost:3000/api/products/order", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({contact, products})
+        })
+        .then(response => {
+            if(response.status === 201) {
+                return response.json()
+            }
+        })
+        .then(data => {
+            window.location.href = `confirmation.html?orderId=${data.orderId}`;
+            let getItem = []
+            localStorage.setItem('items', JSON.stringify(getItem));
+
+        })
+        
+    })
 })
 }
