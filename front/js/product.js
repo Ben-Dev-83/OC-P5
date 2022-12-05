d7bf353041d6e87468a6db627273eee4eaf768ca
Benjamin Bonvicini <benjamin.bonvicini83@gmail.com>
2022-12-05 20:58:35 +0100

    commit avant modifications pour le tri

--- front/js/product.js
+++ front/js/product.js
@@ -1,76 +1,3 @@
-// let str = window.location;
-// let url = new URL(str);
-// let params = new URLSearchParams(url.search);
-// if(params.has("id")) {
-//     params.get('id');
-// }
-// let id = params.get('id');
-
-//  fetch(`http://localhost:3000/api/products/${id}`)
-// .then(response => response.json())
-// .then(data => {
-//     const imgBlock = document.querySelector(".item__img");
-//     imgBlock.innerHTML += `
-//     <img src="${data.imageUrl}" alt="${data.altTxt}">`
-
-//     const title = document.getElementById('title');
-//     title.innerText = data.name;
-    
-//     const price = document.getElementById('price');
-//     price.innerText = data.price;
-    
-//     const description = document.getElementById('description');
-//     description.innerText = data.description;
-
-//     const chooseColor = document.getElementById('colors');
-//         data.colors.map(color => {
-//         const option = document.createElement("option");
-//         option.value = color;
-//         option.innerText = color;
-//         chooseColor.appendChild(option);
-//     })
-    
-//     let itemQuantity = document.getElementById("quantity");
-//     function quantity() {   
-//         return Number(itemQuantity.value);
-//     }
-    
-//     class Item {
-//         constructor(id, color, quantity) {
-//             this.id = id;
-//             this.color = color;
-//             this.quantity = quantity;
-//         }
-//     }
-    
-//     const addToCart = document.getElementById('addToCart');
-//     addToCart.addEventListener('click', () => {
-        
-//         let newItem = new Item(data._id, chooseColor.value, quantity());
-//         let getItem = JSON.parse(localStorage.getItem('items'));
-
-//         if(getItem && newItem.color){
-//             let changeQty = getItem.find(element => element.id == newItem.id && element.color == newItem.color )
-//             if(changeQty) {
-//                 changeQty.quantity = newItem.quantity
-//                 localStorage.setItem('items', JSON.stringify(getItem));
-//             }
-//             else {
-//                 newItem.img = data.imageUrl
-//                 getItem.push(newItem);
-//                 localStorage.setItem('items', JSON.stringify(getItem));
-//             }
-//         }
-//         else if(newItem.color) {
-//             getItem= [];
-//             newItem.img = data.imageUrl
-//             getItem.push(newItem);
-//             localStorage.setItem('items', JSON.stringify(getItem));
-//         }
-//     })
-// })
-
-
 let str = window.location;
 let url = new URL(str);
 let params = new URLSearchParams(url.search);
@@ -120,44 +47,59 @@ let id = params.get('id');
     
     const addToCart = document.getElementById('addToCart');
     addToCart.addEventListener('click', () => {
-        
         let newItem = new Item(data._id, chooseColor.value, quantity());
         let getItem = JSON.parse(localStorage.getItem('items'));
-        let errorMessage = document.querySelector(".errorMessage")
         let itemColor = document.querySelector('.item__content__settings__color')
-        if(!newItem.color || !errorMessage) {
-            itemColor.innerHTML += `<p class="errorMessage">Veuillez entrer une couleur</p>`
-            console.log(errorMessage)
-        }
-        else  {
-            console.log(newItem.color)
-            errorMessage.innerText = 'dbdfbdfb'
-        }
-        
-        if(itemQuantity.value < 1 || itemQuantity.value > 100 && !errorMessage) {
-            let item = document.querySelector('.item__content__settings__quantity')
-            item.innerHTML += `<p class="errorMessage">Veuillez entrer un nombre entre 1 et 100</p>`
-            console.log('testt')
-        }
+        let errorColor = document.createElement('p');
+        let displayErrorColor = document.querySelector('.item__content__settings__color p')
+        let itemQty = document.querySelector('.item__content__settings__quantity')
+        let errorQty = document.createElement('p') 
+        let displayErrorQty = document.querySelector('.item__content__settings__quantity p')
 
-        if(getItem && newItem.color){
-            let changeQty = getItem.find(element => element.id == newItem.id && element.color == newItem.color )
-            if(changeQty) {
-                changeQty.quantity = newItem.quantity
-                localStorage.setItem('items', JSON.stringify(getItem));
+        
+        if((itemQuantity.value < 1 || itemQuantity.value > 100) || !chooseColor.value) {
+
+            if(!displayErrorColor && !chooseColor.value) {
+                errorColor.innerText = "Veuillez selectionner une couleur"
+                itemColor.append(errorColor)
             }
-            else {
+            else if(displayErrorColor !== null && chooseColor.value)  {
+                displayErrorColor.remove();
+            }
+            if(itemQuantity.value < 1 || itemQuantity.value > 100 && !displayErrorQty) {
+                errorQty.innerText = "Veuillez entrer un nombre entre 1 et 100"
+                itemQty.append(errorQty)
+            }
+            else if(itemQuantity.value >= 1 || itemQuantity.value <= 100) {
+                displayErrorQty.remove()
+            }
+        }
+        if(itemQuantity.value >= 1 || itemQuantity.value <= 100 && chooseColor.value) {
+
+            if(getItem && newItem.color && itemQuantity.value >= 1 || itemQuantity.value <= 100){
+                let changeQty = getItem.find(element => element.id == newItem.id && element.color == newItem.color )
+                if(changeQty) {
+                    changeQty.quantity = newItem.quantity
+                    localStorage.setItem('items', JSON.stringify(getItem));
+                }
+                else {
+                    getItem.push(newItem);
+                    localStorage.setItem('items', JSON.stringify(getItem));
+                }
+            }
+            else if(newItem.color  && itemQuantity.value >= 1 || itemQuantity.value <= 100) {
+                getItem= [];
                 newItem.img = data.imageUrl
                 getItem.push(newItem);
                 localStorage.setItem('items', JSON.stringify(getItem));
             }
+            
         }
-        else if(newItem.color) {
-            getItem= [];
-            newItem.img = data.imageUrl
-            getItem.push(newItem);
-            localStorage.setItem('items', JSON.stringify(getItem));
-        }
+
+
+
+        
+       
+
     })
 })
-
