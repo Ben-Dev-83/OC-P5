4c3a721f3c4802a0b8c9e3b2f4fcbc8be3e5f732
Benjamin Bonvicini <benjamin.bonvicini83@gmail.com>
2022-12-27 00:03:31 +0100

    refactorisation du code

--- front/js/script.js
+++ front/js/script.js
@@ -1,12 +1,8 @@
 const callApi = async function () {
-
   fetch("http://localhost:3000/api/products")
   .then(response => response.json())
   .then(data => {
-    
     for(items of data) {
-      
-      
       const itemContainer = document.querySelector('#items');
       itemContainer.innerHTML += `
       <a href="./product.html?id=${items._id}">
@@ -16,27 +12,6 @@ const callApi = async function () {
         <p class="productDescription">${items.description}</p>
       </article>
     </a>`
-      // const a = document.createElement("a");
-      // a.href = `./product.html?id=${items._id}`
-      // itemContainer.appendChild(a);
-      
-      // const article = document.createElement("article");
-      // a.appendChild(article);
-      
-      // const img = document.createElement("img");
-	    // img.src = items.imageUrl;
-	    // img.alt = items.altTxt;
-	    // article.appendChild(img);
-      
-      // const title = document.createElement("h3");
-      // title.classList.add('productName')
-	    // title.innerText = items.name;
-	    // article.appendChild(title);
-      
-      // const p = document.createElement('p')
-      // p.classList.add('description')
-      // p.innerText = items.description;
-      // article.appendChild(p);
     }
     
   })
