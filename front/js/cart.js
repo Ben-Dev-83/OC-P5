c5182eab6210c21280f8dd12e0b0c843ad74edf8
Benjamin Bonvicini <benjamin.bonvicini83@gmail.com>
2022-12-28 08:30:33 +0100

    actualisation du prix après la suppression, message erreur input

--- front/js/cart.js
+++ front/js/cart.js
@@ -5,10 +5,7 @@ for(let item of getItem) {
     await fetch(`http://localhost:3000/api/products/${item.id}`)
     .then(response => response.json())
     .then(data => {
-        data.color = item.color;
-        data.quantity = Number(item.quantity);
-        data.id= item.id;
-        dataList.push(data);
+        dataList.push({...data, color: item.color, quantity: Number(item.quantity), id: item.id});
     })
 };
 const section = document.getElementById("cart__items")
@@ -16,28 +13,31 @@ const section = document.getElementById("cart__items")
 const listItem =  function() {
     for(let item of dataList) {
         section.innerHTML += 
-        `<article class="cart__item" data-id="${item._id}" data-color="${item.color}">
-            <div class="cart__item__img">
-                <img src="${item.imageUrl}" alt="${item.altTxt}">
-            </div>
-            <div class="cart__item__content">
-                <div class="cart__item__content__description">
-                    <h2>${item.name}</h2>
-                    <p>${item.color}</p>
-                    <p>${item.price} €</p>
+            `<article class="cart__item" data-id="${item._id}" data-color="${item.color}">
+                <div class="cart__item__img">
+                    <img src="${item.imageUrl}" alt="${item.altTxt}">
                 </div>
-                <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                        <p>Qté :</p>
-                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" data-id="${item._id}" data-color="${item.color}" value=${Number(item.quantity)}>
+                <div class="cart__item__content">
+                    <div class="cart__item__content__description">
+                        <h2>${item.name}</h2>
+                        <p>${item.color}</p>
+                        <p>${Number(item.price)} €</p>
                     </div>
-                    <div class="cart__item__content__settings__delete">
-                        <p class="deleteItem">Supprimer</p>
+                    <div class="cart__item__content__settings">
+                        <div class="cart__item__content__settings__quantity">
+                            <p>Qté :</p>
+                            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" data-id="${item._id}" data-color="${item.color}" value=${Number(item.quantity)}>
+                        </div>
+                        <div class="cart__item__content__settings__delete">
+                            <p class="deleteItem">Supprimer</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </article>`;
+            </article>`;
     }
+}
+if(dataList.length === 0){
+    section.innerHTML +=`<p>Votre panier est vide</p>`
 }
 dataList.sort((a, b) => {
     if (a._id < b._id) {
@@ -46,17 +46,18 @@ dataList.sort((a, b) => {
     if (a._id > b._id) {
         return 1;
     }
-    return 0
+    return 0;
 })
 listItem()
 
-const articles = document.querySelectorAll('.cart__item')
-const displayTotalPrice = document.getElementById('totalPrice')
-const totalQty = document.getElementById('totalQuantity')
+const articles = document.querySelectorAll('.cart__item');
+const totalQty = document.getElementById('totalQuantity');
+const displayTotalPrice = document.getElementById('totalPrice');
+displayTotalPrice.innerText = 0
 
 const totalQuantity = function() {
-    let totalItemQty = dataList.map(item => item.quantity).reduce((pre, curr) => pre + curr, 0)
-    totalQty.innerHTML = totalItemQty 
+    let totalItemQty = dataList.map(item => item.quantity).reduce((pre, curr) => pre + curr, 0);
+    totalQty.innerHTML = totalItemQty ;
 }
 totalQuantity()
 
@@ -64,26 +65,25 @@ const totalPrice = function() {
     for(let article of articles) {
         let item = dataList.find(element => element.id === article.dataset.id && element.color === article.dataset.color)
         item.subTotal = item.quantity * item.price;
-        let totalItemPrice = dataList.map(item => item.subTotal).reduce((pre, curr) => pre + curr, 0)
-        localStorage.setItem('items', JSON.stringify(dataList));
-        displayTotalPrice.innerHTML = totalItemPrice;
+        displayTotalPrice.innerHTML = dataList.map(item => item.subTotal).reduce((pre, curr) => pre + curr, 0);
     }
 }
 totalPrice()
     
 const changeTotal = function() {
-    let changeQty = document.querySelectorAll('.itemQuantity')
+    let changeQty = document.querySelectorAll('.itemQuantity');
     for(let qty of changeQty) {
         qty.addEventListener('change', (e) => {
-            let item = dataList.find(element => element.id === qty.dataset.id && element.color === qty.dataset.color)
-            item.quantity = Number(e.target.value)
-            let totalItemQty = dataList.map(item => item.quantity).reduce((pre, curr) => pre + curr, 0)
+            const getItem = JSON.parse(localStorage.getItem('items'));
+            let getItemList = getItem.find(element => element.id === qty.dataset.id && element.color === qty.dataset.color);
+            let item = dataList.find(element => element.id === qty.dataset.id && element.color === qty.dataset.color);
+            getItemList.quantity = Number(e.target.value);
+            let totalItemQty = getItem.map(item => item.quantity).reduce((pre, curr) => pre + curr, 0);
             totalQty.innerHTML = totalItemQty;
-            item.subTotal = item.quantity * item.price;
-            let totalItemPrice = dataList.map(item => item.subTotal).reduce((pre, curr) => pre + curr, 0)
-            console.log(dataList)
-            localStorage.setItem('items', JSON.stringify(dataList));
-            displayTotalPrice.innerHTML = totalItemPrice 
+            item.subTotal = getItemList.quantity * item.price;
+            let totalItemPrice = dataList.map(item => item.subTotal).reduce((pre, curr) => pre + curr, 0);
+            localStorage.setItem('items', JSON.stringify(getItem));
+            displayTotalPrice.innerHTML = totalItemPrice;
         })
     }
 }
@@ -94,119 +94,156 @@ const dltSelectItem = function() {
     deleteItems.forEach(deleteItem => {
         deleteItem.addEventListener("click", (e) => {
             e.preventDefault()
-            let dataList = JSON.parse(localStorage.getItem('items'));
-            let articleDlt = deleteItem.closest('article')
-            let itemsFilter = dataList.filter(product => product.id !== articleDlt.dataset.id || product.color !== articleDlt.dataset.color)
+            const getItem = JSON.parse(localStorage.getItem('items'));
+            const articleDlt = deleteItem.closest('article')
+            let dataFilter = dataList.filter(product => product.id !== articleDlt.dataset.id || product.color !== articleDlt.dataset.color)
+            let itemsFilter = getItem.filter(product => product.id !== articleDlt.dataset.id || product.color !== articleDlt.dataset.color)
+            localStorage.setItem('items', JSON.stringify(itemsFilter));
+            dataFilter.subTotal = dataFilter.price * getItem.quantity
             articleDlt.remove()
-            let newItem = itemsFilter
-            localStorage.setItem('items', JSON.stringify(newItem));
-            let totalItemPrice = itemsFilter.map(item => item.subTotal).reduce((pre, curr) => pre + curr, 0)
-            let totalQuantity = itemsFilter.map(item => item.quantity).reduce((pre, curr) => pre + curr, 0)
-            displayTotalPrice.innerHTML = totalItemPrice 
-            totalQty.innerHTML = totalQuantity;
+            displayTotalPrice.innerHTML = dataFilter.map(item => item.subTotal).reduce((pre, curr) => pre + curr, 0) 
+            totalQty.innerHTML = itemsFilter.map(item => item.quantity).reduce((pre, curr) => pre + curr, 0);
+
+            if(itemsFilter.length === 0){
+                return section.innerHTML +=`<p>Votre panier est vide</p>`
+            }
         })
     })
 }
-dltSelectItem()            
-        
-        
-        
-        
-        // let firstName = document.getElementById('firstName')
-    // let firstNameErrorMsg = document.getElementById('firstNameErrorMsg')
-    // firstName.addEventListener('blur', (e) => {
-//     let text = e.target.value
-//     let regex = new RegExp(/^[a-zA-Zéêëèîïâäçù ,'-]{3,20}$/)
-//     if(!regex.test(text)) {
-//         firstNameErrorMsg.innerHTML = "Veuillez saisir votre prénom"
-//     }
-//     else {
-//         firstNameErrorMsg.innerHTML = ""
-//     }
-// })
+dltSelectItem()      
 
-// let lastName = document.getElementById('lastName')
-// let lastNameErrorMsg = document.getElementById('lastNameErrorMsg')
-// lastName.addEventListener('blur', (e) => {
-//     let text = e.target.value
-//     let regex = new RegExp(/^[a-zA-Zéêëèîïâäçù ,'-]{3,20}$/)
-//     if(!regex.test(text)) {
-//         lastNameErrorMsg.innerHTML = "Veuillez saisir votre nom"
-//     }
-//     else {
-//         lastNameErrorMsg.innerHTML = ""
-//     }
-// })
+const firstName = document.getElementById('firstName')
+const lastName = document.getElementById('lastName')
+const address = document.getElementById('address')
+const city = document.getElementById('city')
+const email = document.getElementById('email')
 
-// let address = document.getElementById('address')
-// let addressErrorMsg = document.getElementById('addressErrorMsg')
-// address.addEventListener('blur', (e) => {
-//     let text = e.target.value
-//     let regex = new RegExp(/^[0-9]{1,3}[a-zA-Zéêëèîïâäçù ,'-]{3,30}$/)
-//     if(!regex.test(text)) {
-//         addressErrorMsg.innerHTML = "Veuillez saisir votre adresse"
-//     }
-//     else {
-//         addressErrorMsg.innerHTML = ""
-//     }
-// })
+const firstNameErrorMsg = document.getElementById('firstNameErrorMsg')
+const errorFirstName = function() {
+    firstName.addEventListener('input', (e) => {
+        let regex = new RegExp(/^[a-zA-Zéêëèîïâäçù ,'-]{3,20}$/)
+        if(!regex.test(e.target.value)) {
+            firstNameErrorMsg.style.display = "block"
+            firstNameErrorMsg.innerText = "Veuillez saisir un prénom valide"
+        }
+        if(regex.test(e.target.value) || e.target.value.length === 0) {
+            firstNameErrorMsg.style.display = "none"
+        }
+    })
+} 
+errorFirstName()
 
-// let city = document.getElementById('city')
-// let cityErrorMsg = document.getElementById('cityErrorMsg')
-// city.addEventListener('blur', (e) => {
-//     let text = e.target.value
-//     let regex = new RegExp(/^[a-zA-Zéêëèîïâäçù ,'-]{3,20}$/)
-//     if(!regex.test(text)) {
-//         cityErrorMsg.innerHTML = "Veuillez saisir votre ville"
-//     }
-//     else {
-//         cityErrorMsg.innerHTML = ""
-//     }
-// })
+let lastNameErrorMsg = document.getElementById('lastNameErrorMsg')
+const errorLastName = function() {
+    lastName.addEventListener('input', (e) => {
+        let regex = new RegExp(/^[a-zA-Zéêëèîïâäçù ,'-]{3,20}$/)
+        if(!regex.test(e.target.value)) {
+            lastNameErrorMsg.style.display = "block"
+            lastNameErrorMsg.innerText = "Veuillez saisir un nom valide"
+        }
+        if(regex.test(e.target.value) || e.target.value.length === 0) {
+            lastNameErrorMsg.style.display = "none"
+        }
+    })
+}
+errorLastName()
 
-// let email = document.getElementById('email')
-// let emailErrorMsg = document.getElementById('emailErrorMsg')
-// email.addEventListener('blur', (e) => {
-//     let text = e.target.value
-//     let regex = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
-//     if(!regex.test(text)) {
-//         emailErrorMsg.innerHTML = "Email invalide"
-//     }
-//     else {
-//         emailErrorMsg.innerHTML = ""
-//     }
-// })
+let addressErrorMsg = document.getElementById('addressErrorMsg')
+const errorAddress = function() {
+    address.addEventListener('input', (e) => {
+        let regex = new RegExp(/^[0-9]{1,3}[a-zA-Zéêëèîïâäçù ,'-]{3,30}$/)
+        console.log(regex.test(e.target.value))
+        if(!regex.test(e.target.value)) {
+            addressErrorMsg.style.display = "block"
+            addressErrorMsg.innerText = "Veuillez saisir une adresse valide"
+        }
+        if(regex.test(e.target.value) || e.target.value.length === 0) {
+            addressErrorMsg.style.display = "none"
+        }
+    }) 
+}
+errorAddress()
 
-// let order = document.getElementById('order')
-// order.addEventListener('click', (e) => {
-//     e.preventDefault(e)
-//     let products = dataList.map(product => product.id)
-//     let contact = {
-//         firstName: firstName.value,
-//         lastName: lastName.value,
-//         address: address.value,
-//         city: city.value,
-//         email: email.value
-//     }
+let cityErrorMsg = document.getElementById('cityErrorMsg')
+const errorCity = function() {
+    city.addEventListener('input', (e) => {
+        let regex = new RegExp(/^[a-zA-Zéêëèîïâäçù ,'-]{3,20}$/)
+        if(!regex.test(e.target.value)) {
+            cityErrorMsg.style.display = "block"
+            cityErrorMsg.innerText = "Veuillez saisir votre ville"
+        }
+        if(regex.test(e.target.value) || e.target.value.length === 0) {
+            cityErrorMsg.style.display = "none"
+        }
+    })
+}
+errorCity()
+
+let emailErrorMsg = document.getElementById('emailErrorMsg')
+const errorEmail = function() {
+    email.addEventListener('input', (e) => {
+        let regex = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+        if(!regex.test(e.target.value)) {
+            emailErrorMsg.style.display = "block"
+            emailErrorMsg.innerHTML = "Veuillez saisir un email valide"
+        } 
+        if(regex.test(e.target.value) || e.target.value.length ===0) {
+            emailErrorMsg.style.display = "none"
+        }
+    })
+}
+errorEmail()
+
+let order = document.getElementById('order')
+order.addEventListener('click', (e) => {
+    e.preventDefault(e)
+    errorFirstName()
+    if(firstName.value.length, lastName.value.length, address.value.length, city.value.length, email.value.length  === 0) {
+        firstNameErrorMsg.style.display = "block"
+        firstNameErrorMsg.innerText = "Veuillez saisir votre prénom"
+        lastNameErrorMsg.style.display = "block"
+        lastNameErrorMsg.innerText = "Veuillez saisir votre nom"
+        addressErrorMsg.style.display = "block"
+        addressErrorMsg.innerText = "Veuillez saisir votre adresse"
+        cityErrorMsg.style.display = "block"
+        cityErrorMsg.innerText = "Veuillez saisir votre ville"
+        emailErrorMsg.style.display = "block"
+        emailErrorMsg.innerText = "Email invalide"
+    } else {
+        firstNameErrorMsg.style.display = "none"
+        lastNameErrorMsg.style.display = "none"
+        addressErrorMsg.style.display = "none"
+        cityErrorMsg.style.display = "none"
+        emailErrorMsg.style.display = "none"
+    }
+
+    let products = dataList.map(product => product.id)
+    let contact = {
+        firstName: firstName.value,
+        lastName: lastName.value,
+        address: address.value,
+        city: city.value,
+        email: email.value
+    }
     
-//     fetch("http://localhost:3000/api/products/order", {
-//         method: "POST",
-//         headers: {
-//             'Content-Type': 'application/json',
-//             "Accept": "application/json"
-//         },
-//         body: JSON.stringify({contact, products})
-//     })
-//     .then(response => {
-//         if(response.status === 201) {
-//             return response.json()
-//         }
-//     })
-//     .then(data => {
-//         if(dataList && contact) {
-//             window.location.href = `confirmation.html?orderId=${data.orderId}`;
-//             let dataList = []
-//             localStorage.setItem('itemstest', JSON.stringify(dataList));
-//         }
-//     })
-// })
+    fetch("http://localhost:3000/api/products/order", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({contact, products})
+    })
+    .then(response => {
+        if(response.status === 201) {
+            return response.json()
+        }
+    })
+    .then(data => {
+        if(dataList && contact) {
+            window.location.href = `confirmation.html?orderId=${data.orderId}`;
+            let dataList = []
+            localStorage.setItem('items', JSON.stringify(dataList));
+        }
+    })
+})
